71f68e48024825354ab695c8648efcfaa20fa1ea
Jonathan Sönnerup <Nooxet@gmail.com>
2019-01-12 12:56:44 +0100

    Fixed struct packed declaration, no longer gives warnings about ignoring attribute

--- kernel.h
+++ kernel.h
@@ -5,12 +5,12 @@
 #include <stdint.h>
 #include <stddef.h>
 
-typedef struct {
+typedef struct __attribute__((packed)) {
 	char registers[32];
 	uint16_t sp;
 	uint16_t pc;
 	void(*fp)(void);
-} ctxt_t __attribute__((packed));
+} ctxt_t;
 
 void register_process(void (*)(void));
 void kernel_start();
